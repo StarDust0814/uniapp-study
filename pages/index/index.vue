d96249ada1fd049904a6432aec5a1effbb05c7d5
stardust <876700669@qq.com>
2024-07-21 16:32:22 +0800

    add:scrollview实现纵向和横向元素排列

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,6 +4,32 @@
 	</view>
 	<view>-----</view>
 	<text selectable user-select>支持选中的text标签包裹文本</text>
+	<view>-----</view>
+	<scroll-view scroll-x class="scrollView">
+		<view class="scrollBox">scroll子元素</view>
+		<view class="scrollBox">scroll子元素</view>
+		<view class="scrollBox">scroll子元素</view>
+		<view class="scrollBox">scroll子元素</view>
+		<view class="scrollBox">scroll子元素</view>
+		<view class="scrollBox">scroll子元素</view>
+		<view class="scrollBox">scroll子元素</view>
+		<view class="scrollBox">scroll子元素</view>
+		<view class="scrollBox">scroll子元素</view>
+		<view class="scrollBox">scroll子元素</view>
+		<view class="scrollBox">scroll子元素</view>
+	</scroll-view>
+	<view>-----</view>
+	<scroll-view scroll-y class="scrollView">
+		<view>scroll子元素</view>
+		<view>scroll子元素</view>
+		<view>scroll子元素</view>
+		<view>scroll子元素</view>
+		<view>scroll子元素</view>
+		<view>scroll子元素</view>
+		<view>scroll子元素</view>
+		<view>scroll子元素</view>
+		<view>scroll子元素</view>
+	</scroll-view>
 </template>
 
 <script setup>
@@ -27,5 +53,19 @@
 	}
 	.innerHover{
 		background: greenyellow;
+	}
+	.scrollView{
+		width: 80%;
+		height: 220px;
+		border: 1px solid red;
+		// 不允许换行
+		white-space: nowrap;
+		.scrollBox{
+			width: 100px;
+			height: 100px;
+			background: green;
+			// 块横向排列
+			display: inline-block;
+		}
 	}
 </style>
