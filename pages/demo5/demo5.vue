cfbed269adb1c27ecc79061ef5ba4e173dbff6c3
stardust <876700669@qq.com>
2024-07-25 10:54:49 +0800

    add:uniapp request

---
+++ pages/demo5/demo5.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="layout">{{ arrs }}</view>
+</template>
+
+<script setup>
+let arrs = ref([]);
+function request() {
+	uni.request({
+		url: 'https://jsonplaceholder.typicode.com/posts',
+		success: (res) => {
+			console.log(res);
+			arrs.value = res.data;
+		}
+	});
+}
+request();
+</script>
+
+<style lang="scss" scoped></style>
