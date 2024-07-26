53526a018a6283802456b131930233ed9b702a7a
stardust <876700669@qq.com>
2024-07-26 09:31:23 +0800

    add:图片内容填充

--- pages/requestDemo/requestDemo.vue
+++ pages/requestDemo/requestDemo.vue
@@ -1,17 +1,33 @@
 <template>
 	<view class="container">
 		<view class="layout">
-			<view class="box" v-for="item in 5">
+			<view class="box" v-for="item in pets" :key="item._id">
 				<view class="pic">
-					<image src="../../static/logo.png" mode="widthFix"></image>
+					<image :src="item.url" mode="widthFix"></image>
 				</view>
-				<view class="text">@@@</view>
+				<view class="text">{{ item.content }}</view>
+				<view class="author">—— {{ item.author }}</view>
 			</view>
 		</view>
 	</view>
 </template>
 
-<script setup></script>
+<script setup>
+const pets = ref([]);
+// URL附加的参数可以填在 data字段
+function network() {
+	uni.request({
+		url: 'https://tea.qingnian8.com/tools/petShow',
+		data: {
+			size: 10
+		}
+	}).then((res) => {
+		console.log(res);
+		pets.value = res.data.data;
+	});
+}
+network();
+</script>
 
 <style lang="scss" scoped>
 .container {
@@ -27,6 +43,14 @@
 			}
 			.text {
 				padding: 5rpx;
+				color: #333;
+				font-size: 36rpx;
+			}
+			.author {
+				padding: 0 30rpx 30rpx;
+				text-align: right;
+				color: #888;
+				font-size: 24rpx;
 			}
 		}
 	}
